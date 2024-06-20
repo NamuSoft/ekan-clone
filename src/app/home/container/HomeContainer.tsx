0fbb6c23c68d0773e64659da47aa7326a7c08fbf
Spacedaway <ekanabasiisok@gmail.com>
2024-06-20 15:29:55 +0100

    feat: add home container with props

--- src/app/home/container/HomeContainer.tsx
+++ src/app/home/container/HomeContainer.tsx
@@ -1,9 +1,20 @@
 import { HomeTemplate } from "~/components/Templates/Home/HomeTemplate";
 
 export const HomeContainer = () => {
-
   const homeTemplateProps: React.ComponentProps<typeof HomeTemplate> = {
-    homeContentModuleProps: { title: "HomeContentModule" },
+    homeContentModuleProps: {
+      darkLightProps: [
+        {
+          imageUrl: "/assets/1.png",
+          title: "Dark Mode",
+        },
+        {
+          imageUrl: "/assets/2.png",
+          title: "Light Mode",
+        },
+      ],
+    },
+    linesAtomProps: { numberOfLines: 5 },
   };
 
   return <HomeTemplate {...homeTemplateProps} />;
