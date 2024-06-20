e0f51d9e58435b0363022f913f87d474758a25a0
Spacedaway <ekanabasiisok@gmail.com>
2024-06-20 15:32:43 +0100

    feat: add footer atom

---
+++ src/components/Atoms/FooterAtom/FooterAtom.tsx
@@ -0,0 +1,14 @@
+export function FooterAtom() {
+  return (
+    <div className='text-center'>
+      <p className='text-base text-white'>
+        © 2023 Gavi is Proudly Powered by{" "}
+        <span className='text-[#c8f31d] underline'>
+          <a href='https://themeforest.net/user/UiCamp' target='_blank'>
+            UiCamp
+          </a>
+        </span>
+      </p>
+    </div>
+  );
+}
