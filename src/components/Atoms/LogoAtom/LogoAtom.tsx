16cc47c8ec74a7c2cfd0ef40e3ba396c40ce5441
Spacedaway <ekanabasiisok@gmail.com>
2024-06-21 16:47:53 +0100

    feat: add dark logo

--- src/components/Atoms/LogoAtom/LogoAtom.tsx
+++ src/components/Atoms/LogoAtom/LogoAtom.tsx
@@ -1,13 +1,38 @@
-import Link from "next/link";
+"use client";
 
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useTheme } from "next-themes";
 import { Image } from "antd";
 
 export function LogoAtom() {
+  const [mounted, setMounted] = useState(false);
+  const { theme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return <div />;
+
+  if (theme === "dark") {
+    return (
+      <div>
+        <Link href='#0'>
+          <Image
+            src='assets/logo-light.png'
+            alt='Logo'
+            width={60}
+            preview={false}
+          />
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Link href='#0'>
         <Image
-          src='assets/logo-light.png'
+          src='assets/logo-dark.png'
           alt='Logo'
           width={60}
           preview={false}
