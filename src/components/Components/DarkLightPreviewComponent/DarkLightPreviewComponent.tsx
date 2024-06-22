bed16c2c245169a5553a5ae4934731227ca57814
Spacedaway <ekanabasiisok@gmail.com>
2024-06-22 12:29:08 +0100

    refactor: change to next image

--- src/components/Components/DarkLightPreviewComponent/DarkLightPreviewComponent.tsx
+++ src/components/Components/DarkLightPreviewComponent/DarkLightPreviewComponent.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { Image } from "antd";
 
 import styles from "./DarkLightPreviewComponent.module.css";
+import Image from "next/image";
 
 type Props = {
   imageUrl: string;
@@ -15,7 +15,13 @@ export function DarkLightPreviewComponent({ imageUrl, title, route }: Props) {
       <div className='z-[3] p-[2.5rem]'>
         <div className='h-[150] overflow-hidden rounded-[1rem_1rem_0_0] border border-b-0 border-solid border-[#aaa]'>
           <Link href={route} className='pointer-events-auto' target='blank'>
-            <Image src={imageUrl} alt='' preview={false} />
+            <Image
+              src={imageUrl}
+              alt=''
+              width={500}
+              height={0}
+              style={{ width: "100%", height: "auto" }}
+            />
           </Link>
         </div>
         <div className='-ml-10 h-[1.875rem] w-[calc(100%_+_5rem)] rounded-[0_0_6.25rem_6.25rem] border border-solid border-[#aaa]' />
