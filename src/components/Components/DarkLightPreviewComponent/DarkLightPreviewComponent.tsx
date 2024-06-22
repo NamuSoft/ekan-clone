9061de229f869e5502b58f3552589264e83958bf
Spacedaway <ekanabasiisok@gmail.com>
2024-06-22 12:40:18 +0100

    style: add relative position

--- src/components/Components/DarkLightPreviewComponent/DarkLightPreviewComponent.tsx
+++ src/components/Components/DarkLightPreviewComponent/DarkLightPreviewComponent.tsx
@@ -12,16 +12,18 @@ type Props = {
 export function DarkLightPreviewComponent({ imageUrl, title, route }: Props) {
   return (
     <div className={styles.item}>
-      <div className='z-[3] p-[2.5rem]'>
+      <div className='relative p-[2.5rem]'>
         <div className='h-[150] overflow-hidden rounded-[1rem_1rem_0_0] border border-b-0 border-solid border-[#aaa]'>
           <Link href={route} className='pointer-events-auto' target='blank'>
-            <Image
-              src={imageUrl}
-              alt=''
-              width={500}
-              height={0}
-              style={{ width: "100%", height: "auto" }}
-            />
+            <div>
+              <Image
+                src={imageUrl}
+                alt=''
+                width={500}
+                height={0}
+                style={{ width: "100%", height: "auto" }}
+              />
+            </div>
           </Link>
         </div>
         <div className='-ml-10 h-[1.875rem] w-[calc(100%_+_5rem)] rounded-[0_0_6.25rem_6.25rem] border border-solid border-[#aaa]' />
